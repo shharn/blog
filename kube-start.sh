bdf8301cb924a56582757173ff1f8f425b8b2dac
Seunghwan Han <shharn2@gmail.com>
2018-12-19 16:29:22 +0900

    Working on setting CI/CD

--- kube-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-kubectl create -f ./client/kubernetes/*
-echo Client pod & service successfully created
-kubectl create -f ./server/kubernetes/*
-echo Server/Database pods & services successfully created
-kubectl create -f ./ingress.dev.yaml
-echo Local Ingress object successfully created
-echo If you wanna run app correctly,\nset host 'puppyloper.blog', 'api.puppyloper.blog' to minikube IP Address
