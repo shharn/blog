fbbdc18a1045644d5c3cdb86f9ea15ba7967bcdd
Seunghwan Han <shharn2@gmail.com>
2018-12-23 10:57:17 +0900

    Resolve webpack bundling issue

--- client/front/config/webpack.config.prod.js
+++ client/front/config/webpack.config.prod.js
@@ -272,7 +272,7 @@ module.exports = {
         // https://github.com/facebookincubator/create-react-app/issues/2488
         ascii_only: true,
       },
-      sourceMap: shouldUseSourceMap,
+      sourceMap: false,
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin({
