e78de4824a417c83e55698940e895c173d71f693
Seunghwan Han <shharn2@gmail.com>
2019-01-05 15:31:14 +0900

    Add cert-manager to cluster for ssl certificate

--- client/front/src/service/index.js
+++ client/front/src/service/index.js
@@ -3,9 +3,11 @@ import request from 'superagent';
 import { HEADER_NAME_FOR_TOKEN } from '../constant';
 import type { LoginInformation } from '../flowtype';
 
+const PROTOCOL = window.location.protocol;
+
 export function requestLogin(loginInfo: LoginInformation): Promise<request.Response | Error> {
     return request
-            .post(`http://${process.env.API_SERVER_URL}/login`)
+            .post(`${PROTOCOL}//${process.env.API_SERVER_URL}/login`)
             .type('text/plain')
             .accept('json')
             .send(JSON.stringify(loginInfo))
@@ -15,7 +17,7 @@ export function requestLogin(loginInfo: LoginInformation): Promise<request.Respo
 
 export function validateToken(token: string): Promise<request.Response | Error> {
     return request
-        .get(`http://${process.env.API_SERVER_URL}/check`)
+        .get(`${PROTOCOL}//${process.env.API_SERVER_URL}/check`)
         .set(HEADER_NAME_FOR_TOKEN, token)
         .type('text/plain')
         .accept('json')
@@ -25,7 +27,7 @@ export function validateToken(token: string): Promise<request.Response | Error> 
 
 export function requestLogout(token: string): Promise<request.Response | Error> {
     return request
-        .post(`http://${process.env.API_SERVER_URL}/logout`)
+        .post(`${PROTOCOL}//${process.env.API_SERVER_URL}/logout`)
         .set(HEADER_NAME_FOR_TOKEN, token)
         .then(res => res)
         .catch(err => err.response ? err.response : err);
@@ -33,7 +35,7 @@ export function requestLogout(token: string): Promise<request.Response | Error> 
 
 export function getData(dataName: string): Promise<request.Response | Error> {
     return request
-        .get(`http://${process.env.API_SERVER_URL}/${dataName}`)
+        .get(`${PROTOCOL}//${process.env.API_SERVER_URL}/${dataName}`)
         .accept('json')
         .then(res => res)
         .catch(err => err.response ? err.response : err);
@@ -41,7 +43,7 @@ export function getData(dataName: string): Promise<request.Response | Error> {
 
 export function getDataWithURL(url: string): Promise<request.Response | Error> {
     return request
-        .get(`http://${process.env.API_SERVER_URL}/${url}`)
+        .get(`${PROTOCOL}//${process.env.API_SERVER_URL}/${url}`)
         .accept('json')
         .then(res => res)
         .catch(err => err.response ? err.response : err);
@@ -49,7 +51,7 @@ export function getDataWithURL(url: string): Promise<request.Response | Error> {
 
 export function createData(dataName: string, data: any, token: string): Promise<request.Response | Error> {
     return request
-        .post(`http://${process.env.API_SERVER_URL}/${dataName}`)
+        .post(`${PROTOCOL}//${process.env.API_SERVER_URL}/${dataName}`)
         .set(HEADER_NAME_FOR_TOKEN, token)
         .type('text/plain')
         .accept('json')
@@ -60,7 +62,7 @@ export function createData(dataName: string, data: any, token: string): Promise<
 
 export function updateData(dataName: string, data: any, token: string): Promise<request.Response | Error> {
     return request
-        .patch(`http://${process.env.API_SERVER_URL}/${dataName}/${data.uid}`)
+        .patch(`${PROTOCOL}//${process.env.API_SERVER_URL}/${dataName}/${data.uid}`)
         .set(HEADER_NAME_FOR_TOKEN, token)
         .type('text/plain')
         .accept('json')
@@ -71,7 +73,7 @@ export function updateData(dataName: string, data: any, token: string): Promise<
 
 export function deleteData(dataName: string, uid: string, token: string): Promise<request.Response | Error> {
     return request
-        .delete(`http://${process.env.API_SERVER_URL}/${dataName}/${uid}`)
+        .delete(`${PROTOCOL}//${process.env.API_SERVER_URL}/${dataName}/${uid}`)
         .set(HEADER_NAME_FOR_TOKEN, token)
         .type('text/plain')
         .accept('json')
