29c9e3c82354452e13fcf2460e7bb3755f38a566
Seunghwan Han <shharn2@gmail.com>
2020-06-18 19:07:02 +0900

    add google analytics

--- client/front-server/src/server.js
+++ client/front-server/src/server.js
@@ -90,20 +90,16 @@ app.get('/oauth/authorizations/:platform/callback', (req, res) => {
     const templatePlaceholder = '$$PLACEHOLDER';
     let template = `
     <html>
-        <head>
-            <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K64ZBVD');</script>
-            <!-- End Google Tag Manager -->
-        </head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169931142-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'UA-169931142-1');
+        </script>
         <body>
-            <!-- Google Tag Manager (noscript) -->
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K64ZBVD"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-            <!-- End Google Tag Manager (noscript) -->
             <script>
                ${templatePlaceholder}
             </script>
